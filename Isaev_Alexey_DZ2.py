473691987f0199e3d213069d0c91774dc3b0b604
Isaev Aleksey <isaev.alex75311@gmail.com>
2019-11-10 20:38:09 +0300

    stage 3

--- Isaev_Alexey_DZ2.py
+++ Isaev_Alexey_DZ2.py
@@ -2,6 +2,9 @@
 # об отдельном товаре. В кортеже должно быть два элемента — номер товара и словарь с параметрами (характеристиками товара:
 # название, цена, количество, единица измерения). Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
 
+# Необходимо собрать аналитику о товарах. Реализовать словарь, в котором каждый ключ — характеристика товара,
+# например название, а значение — список значений-характеристик, например список названий товаров.
+
 list = []
 
 
@@ -11,12 +14,24 @@ def checker(op, str):
             result = op(input(str))
             break
         except ValueError:
+            print('Введено некорректное значение')
             continue
     return result
 
 
+def analiz(my_list):
+    result = {}
+    for n in range(len(my_list)):
+        for key, el in my_list[n][1].items():
+            result.setdefault(key, []).append(el)
+    for key, el in result.items():
+        print('{}: {}'.format(key, el))
+
+
 def dict_add():
-    name = input('Введите название товара ')
+    name = ''
+    while not name:
+        name = (input('Введите название товара ')).strip()
     price = checker(float, 'Введите цену товара ')
     counts = checker(int, 'Введите количество товара ')
     units = input('Введите единицу измерения товара ')
@@ -26,9 +41,12 @@ def dict_add():
 
 while True:
     cont = input('Для завершения ввода карточек товара введите N, для продолжения нажмите Enter ').lower();
-    if cont == 'n':
+    if cont == 'n' or cont == 'т':
         break
     else:
         my_dict = dict_add()
         list.append((len(list) + 1, my_dict))
 print(*list, sep='\n')
+
+print('\n\nАналитика по товарам')
+analiz(list)
